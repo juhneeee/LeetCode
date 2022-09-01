c1a8017fe0069aa0290d6fa5bfc90c09d98fc383
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-01 14:56:53 -0400

    Time: 94 ms (44.40%), Space: 42.5 MB (73.40%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    
+    let first = -1
+    
+    while (left <= right && first == -1){
+        const mid = ~~((left + right)/2)
+        
+        if (nums[mid] == target && nums[mid-1] != target){
+            first = mid
+        }
+        if (nums[mid] < target){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    } 
+    if(first == -1){
+        return [-1,-1]
+    }
+    
+    left = 0
+    right = nums.length - 1
+    
+    let last = first
+    
+    while (left <= right){
+        const mid = ~~((left + right)/2)
+        
+        if (nums[mid] == target && nums[mid+1] != target){
+            last = mid
+        }
+        if (nums[mid] <= target){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    } 
+    return [first,last]
+};
