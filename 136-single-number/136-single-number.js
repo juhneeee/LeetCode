b28897425f1d0a92b09fde7f13886fa7451a0e86
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-01 00:41:13 -0400

    Time: 71 ms (95.10%), Space: 42.8 MB (97.19%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    const freq = new Set()
-    nums.forEach(element => {
-        freq.has(element) ? freq.delete(element):freq.add(element) 
-    })
-    for (let a of freq){
-        return a
-    }
-};
+    return nums.reduce((a,b)=> a ^ b)
+}
