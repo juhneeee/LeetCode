95a1189c754e718fbf8f3ff646074658917960fc
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-02 01:21:42 -0400

    Time: 114 ms (9.75%), Space: 44.1 MB (5.13%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// iterate over array creating a table of max earning possible
+var rob = function(nums) {    
+    const memo= {}
+    const len = nums.length
+    let n = 0
+    
+    if (len==1){return nums[0]}
+
+    
+    while (n < len){
+        if (n==0 || n==1){
+            memo[n] = nums[n]
+        } else {
+            const val = Math.max(memo[n-2]+ nums[n],
+                                (memo[n-3] || 0)+nums[n])
+            memo[n] = val
+        }
+        n++
+    }
+    console.log(memo)
+    return Math.max(memo[len-1], memo[len-2])
+    
+};
