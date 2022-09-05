995fa9dbb0097f4b46d1e0bf9f6cd34dcc946a1d
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-05 01:10:56 -0400

    Time: 79 ms (93.07%), Space: 45.8 MB (48.65%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn = function(nums) {
+    const hash = {}
+    let min = 10000
+    let max = 0
+    const memo = {}
+    
+    nums.forEach(num =>{
+        hash[num] = hash[num] || 0
+        hash[num] += num
+        if (num < min) min= num
+        if (num > max) max= num
+    })
+    for(let i=min; i<=max; i++){
+        if (i==min || i==min+1){
+            memo[i] = hash[i] || 0
+        } else {
+            const val = Math.max(memo[i-2] + (hash[i] || 0),
+                                (memo[i-3] || 0)+(hash[i] || 0))
+            memo[i] = val
+        }
+    }
+    return Math.max(memo[max], memo[max-1] || 0)
+};
