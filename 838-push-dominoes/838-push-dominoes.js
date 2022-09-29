20169656016157f2986d2e558dff9f00fa6a1ffb
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-28 22:38:39 -0400

    Time: 433 ms (20.60%), Space: 80.2 MB (11.65%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} dominoes
+ * @return {string}
+ */
+var pushDominoes = function(dominoes) {
+    ".L.R...LR..L.."
+    ".L/./R...L/R..L/.."
+    let newStr = dominoes.replaceAll("L","L,").replaceAll("R",",R")
+    let arr = newStr.split(",")
+    console.log(arr)
+    
+    let result = ""
+    for(let i=0; i<arr.length;i++){
+        const len = arr[i].length,
+              boolL = /L/.test(arr[i]),
+              boolR = /R/.test(arr[i]);
+        if (boolL && boolR){
+            let str = len%2 ? "." : "";
+            for (let i=1; i<= len/2; i++){
+                str = "R"+str+"L"
+            }
+            result+= str
+        } else if (boolL || boolR) {
+            let char = boolL ? "L" : "R",
+                str = "";
+            for(let i=0; i<len; i++){
+                str += char
+            }
+            result+= str
+        } else {
+            result += arr[i]
+        }
+    }
+    console.log(result)
+    return result
+};
