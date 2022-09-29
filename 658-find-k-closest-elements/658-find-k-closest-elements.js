0a2aac63744a07a89e4c0f35f827ebe21f557a32
Johnny Chen <101293362+juhneeee@users.noreply.github.com>
2022-09-29 18:42:22 -0400

    Time: 171 ms (33.54%), Space: 49.1 MB (49.09%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.js
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -7,11 +7,25 @@
 var findClosestElements = function(arr, k, x) {
     // x is target
     // k is quantity
-    arr.sort((a,b)=> Math.abs(a-x)-Math.abs(b-x))
-    console.log(arr)
-    const result = arr.slice(0,k)
-    console.log(result)
-    result.sort((a,b)=>a-b)
-    console.log(result)
-    return result
+    let minSum = null,
+        minIndex = 0,
+        sum = 0;
+    
+    for(let i=0; i<k; i++){
+        sum += Math.abs(arr[i]-x)
+    }
+    minSum = sum
+
+    for(let i=k; i< arr.length; i++){
+        sum += Math.abs(arr[i]-x) - Math.abs(arr[i-k]-x)
+        if (sum < minSum){
+            minSum = sum
+            minIndex = i-k+1
+        }
+    }
+    const res = []
+    for (let i = minIndex; i< minIndex+k; i++){
+        res.push(arr[i])
+    }
+    return res
 };
